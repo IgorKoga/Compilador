7655782c288767c875f5209213100a2ff2b6fb3d
IgorKoga <ihenriquekogavigato@gmail.com>
2026-05-19 00:23:43 -0300

    codigos em rust para base do analisador

---
+++ codigoRust/soma.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let a = 10;
+    let b = 20;
+    let resultado = a + b;
+    println!("A soma de {} com {} é {}", a, b, resultado);
+}
---
+++ codigoRust/string.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let frase_sabedoria _oriental = "Todos podemos escutar, porem apenas quem ouve evolui";
+    let dia_sabedoria_oriental = "20-02-26";
+
+    if dia_sabedoria_oriental == "20-02-26" {
+        println!("{}", frase_sabedoria_oriental);
+    }
+}
