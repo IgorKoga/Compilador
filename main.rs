d952b7880e1924744697fe1511939c20489074ee
IgorKoga <ihenriquekogavigato@gmail.com>
2026-06-17 22:32:20 -0300

    adição: leitura de arquivo de entrada

--- main.rs
+++ main.rs
@@ -25,4 +25,28 @@ fn main() {
     }
     //se não for nenhum dos dois nomes, executa tudo como um unico compilador
     let run_both = !is_lexico_only && !is_sintatico_only;
+
+    let folder = "codigoRust/"; //endereço padrão
+    print!("Digite o nome do arquivo: ");
+    let _= io::stdout().flush();
+
+    let mut file_name = String::new();
+    if let Err(e) = io::stdin().read_line(&mut file_name){`
+        eprintln!("Erro ao ler entrada: {}", e);
+        return;
+    }
+    let file_name = file_name.trim(); //remove poluição do buffer
+    let full_path = format!("{}{}", folder, file_name);
+
+    //abrir e ler arquivo
+    let code = match fs::read_to_string(&full_path){
+        Ok(content) => content,
+        Err(_) => {
+            eprintln!("Erro: O arquivo '{}' nao foi encontrado na pasta!", full_path);
+            println!("\nPressione Enter para fechar a janela...");
+            let mut temp = String::new();
+            let _ = io::stdin().read_line(&mut temp);
+            std::process::exit(1);
+        }
+    };
 }
