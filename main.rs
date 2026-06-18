b3041c23a245d70e2f3c04e359c27029ff09d874
IgorKoga <ihenriquekogavigato@gmail.com>
2026-06-17 22:05:34 -0300

    Adição: Adição de verificação de nome de arquivos para executar features especificas

--- main.rs
+++ main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::path::Path;
+
+mod ast;
+mod lexico;
+mod sintatico;
+
+use lexico::{token_type_to_string, Scanner, TokenType};
+use sintatico::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    //Verifica se o executável é o "lexico" ou "sintatico"
+    //para definir quais partes do código serão executadas.
+    let args: Ver<String> = env::args().collect();
+    let exec_name = args.get(0).cloned().unwrap_or_default();
+
+    let mut is_lexico_only = exec_name.contains("lexico");
+    let is_sintatico_only = exec_name.contains("sintatico");
+
+    #[cfg(feature = "lexico_only")]
+    {
+        is_lexico_only = true;
+    }
+    //se não for nenhum dos dois nomes, executa tudo como um unico compilador
+    let run_both = !is_lexico_only && !is_sintatico_only;
 }
