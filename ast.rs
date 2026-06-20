97c5aef993c24820ccf849e2570f8a275d7e7998
Anna Tsurushima <anna.tsurushima@discente.uenp.edu.br>
2026-06-20 18:57:54 -0300

    feat: implementa metodos to_json e print para Expressoes

--- ast.rs
+++ ast.rs
@@ -25,3 +25,34 @@ pub enum Expr {
         right: Box<Expr>, 
     },
 }
+
+//Métodos das Expressões
+impl Expr {
+    pub fn to_json(&self) -> String {
+        match self { 
+            Expr::Number(val) => format!(r#"{{"type": "Number", "value": {}}}"#, val),
+            Expr::Float(val) => format!(r#"{{"type": "Float", "value": {}}}"#, val),
+            Expr::String(val) => format!(r#"{{"type": "String", "value": "{}"}}"#, escape_json(val)),
+            Expr::Identifier(name) => format!(r#"{{"type": "Identifier", "name": "{}"}}"#, name),
+            Expr::Binary { left, op, right } => format!(
+                r#"{{"type": "BinaryExpr", "op": "{}", "left": {}, "right": {}}}"#,
+                op, left.to_json(), right.to_json() 
+            ),
+        }
+    }
+
+    pub fn print(&self, indent: usize) {
+        let spaces = " ".repeat(indent); 
+        match self {
+            Expr::Number(val) => println!("{}NumberExpr: {}", spaces, val),
+            Expr::Float(val) => println!("{}FloatExpr: {}", spaces, val),
+            Expr::String(val) => println!("{}StringExpr: \"{}\"", spaces, escape_json(val)),
+            Expr::Identifier(name) => println!("{}IdentifierExpr: {}", spaces, name),
+            Expr::Binary { left, op, right } => {
+                println!("{}BinaryExpr ({})", spaces, op);
+                left.print(indent + 2); 
+                right.print(indent + 2);
+            }
+        }
+    }
+}
